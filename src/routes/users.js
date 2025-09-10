2eb48dfff69d409f6c1ad9025afa5b41f9b38111
shubham <shubham@logicfirst.io>
2025-09-10 09:09:28 +0530

    referral link added

--- src/routes/users.js
+++ src/routes/users.js
@@ -44,7 +44,7 @@ router.get('/refercode', authenticate, userController.referral);  // done. // do
 
 router.get('/referlink', authenticate, userController.referlink);  // done
 
-router.post('/addmoney', authenticate, userController.addmoney);  // done , razorPay pending 
+router.post('/addmoney', authenticate, userController.addmoney);  // done , razorPay pending
 router.post('/payment/verify', authenticate, userController.verifypayment);   // razorPay pending
 
 router.post('/referrallink/:referral', authenticate, userController.referrallink);  // done
